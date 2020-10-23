2fa5df0e66a8638187d3ab1592383f166eb32a94
Laura Kubiliene <lauroslapai1@gmail.com>
2020-10-23 13:24:52 +0000

    making github search working

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,11 +1,56 @@
-$("#search-form").submit(function(e) {
-    e.preventDefault ()
+function userInformationHTML (user) {
+    return `
+            <h2>${username}
+                <span class="small-name">
+                    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+                </span>
+            </h2>
+            <div class="gh-content">
+                <div class="gh-avatar">
+                    <a href="${user.html_url}" target="_blank">
+                        <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
+                    </a>
+                </div>
+            </div>`
+}
 
-    var query = $("#search").val()
 
-    var url = "http://openlibrary.org/search.json?q=harry+potter"
 
-    $.get(url,function(data) {
-        console.log(data)
-    })
-})
+
+
+
+function fetchGitHubInformation(event) {
+    
+
+    var username = $("#gh-username").val();
+    if(!username) {
+        $("#gh-user-data").html(`<h2>Please enter the keyword</h2>`);
+        return;
+
+    }
+
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));     
+        }, function(errorResponse) {
+            if (errorRespose.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No Info Found ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorRespose.responseJSON.message}</h2>`);
+            }
+
+        })
+    
+
+
+
+
+
+}
