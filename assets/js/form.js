4e4e9d9d043d7698ba0644f01f3cee46b5a68623
Laura Kubiliene <lauroslapai1@gmail.com>
2020-11-18 13:55:53 +0000

    emails going through!

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,7 +2,7 @@
     emailjs.send("gmail", "template_rq2dfbn", {
         from_name: helpForm.name.value,
         from_email: helpForm.emailaddress.value,
-        message: helpForm.issueReport.value
+        message: helpForm.issuereport.value
         
             
     })
