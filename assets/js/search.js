1268ba582ec1a16018d56288530eb87b57848852
Laura Kubiliene <lauroslapai1@gmail.com>
2020-11-19 16:36:54 +0000

    calling api and it is working

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,12 @@
 function getInformation(){
     document.getElementById(`output`).innerHTML="";
-    fetch("http://openlibrary.org/search.json?q="+document.getElementById(`input`).value)
-    createCanvas(200,200)
+    fetch("https://openlibrary.org/search.json?q="+document.getElementById(`library_search`).value)
     .then(a=>a.json())
     .then(response=>{
         for(var i=0;i<5;i++){
-            document.getElementById(`output`).innerHTML+="<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name[0]+"<br><img src=`http://covers.openlibrary.org/b/isb"+response.docs[i].isbn[0]+"-M.jpg`/><br>"
+            document.getElementById(`output`).innerHTML+="<h2>"+response.docs[i].title[0]+"</h2>"+response.docs[i].author_name[0]
         }
-    })
+    });
    
 }
    
