186e13c3c4d67e04cfb163e47086bb722805762d
Laura Kubiliene <lauroslapai1@gmail.com>
2020-12-04 13:02:25 +0000

    solved the title author problem

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,8 +3,8 @@ function getInformation(){
     fetch("https://openlibrary.org/search.json?q="+document.getElementById(`library_search`).value)
     .then(a=>a.json())
     .then(response=>{
-        for(var i=0;i<5;i++){
-            document.getElementById(`output`).innerHTML+="<h2>"+response.docs[i].title[0]+"</h2>"+response.docs[i].author_name[0]
+        for(var i=0;i<10;i++){
+            document.getElementById(`output`).innerHTML+="<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name
         }
     });
    
