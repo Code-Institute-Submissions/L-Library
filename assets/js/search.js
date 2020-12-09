63227f34bb714332b1722e30db84205a25a3f1eb
Laura Kubiliene <lauroslapai1@gmail.com>
2020-12-09 12:32:00 +0000

    adding some html to js

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,7 +4,7 @@ function getInformation(){
     .then(a=>a.json())
     .then(response=>{
         for(var i=0;i<5;i++){
-            document.getElementById(`output`).innerHTML+=`<h4 class="title">`+response.docs[i].title+`<h4 class="author">`+response.docs[i].author_name+`<h4 class="first">`+response.docs[i].first_publish_year+`<h4 class="publisher">`+response.docs[i].publisher
+            document.getElementById(`output`).innerHTML+=`<h4 class="title">Title:</h4>`+response.docs[i].title+`<h4 class="author">Author:</h4>`+response.docs[i].author_name+`<h4 class="first">First Time Published In:</h4>`+response.docs[i].first_publish_year+`<h4 class="publisher">Publisher:</h4>`+response.docs[i].publisher
         }
     });
    
