726168a42feb72f9e08f10dc8da397217283720b
Laura Kubiliene <lauroslapai1@gmail.com>
2020-12-09 13:33:08 +0000

    tidying the search result

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,7 +4,7 @@ function getInformation(){
     .then(a=>a.json())
     .then(response=>{
         for(var i=0;i<5;i++){
-            document.getElementById(`output`).innerHTML+=`<h4 class="title">Title:`+response.docs[i].title+`<h4 class="author">Author:`+response.docs[i].author_name+`<h4 class="first">First Time Published In:`+response.docs[i].first_publish_year+`<h4 class="publisher">Publisher:`+response.docs[i].publisher
+            document.getElementById(`output`).innerHTML+=`<h4 class="title">Title: `+response.docs[i].title+`<h4 class="author">Author: `+response.docs[i].author_name+`<h4 class="first">First Time Published In: `+response.docs[i].first_publish_year+`<h4 class="publisher">Publisher: `+response.docs[i].publisher
         }
     });
    
